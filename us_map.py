2dbb475da1e430a86bcba06c3be46e1470ca2ac4
Daniel Lessenden <dless27@gmail.com>
2021-01-02 14:31:55 -0600

    Add a dynamic loop to create a marker for every volcano in the US

--- us_map.py
+++ us_map.py
@@ -1,11 +1,21 @@
 import folium
+import pandas
+
+data = pandas.read_csv("volcano.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
 
 map = folium.Map(location=[38, -99], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for coords in [[38, -99], [37, -97]]:
-    fg.add_child(folium.Marker(location=coords, popup="Oh Hello!", icon=folium.Icon(color='green')))
+# for coords in [[38, -99], [37, -97]]:
+#     fg.add_child(folium.Marker(location=coords, popup="Oh Hello!", icon=folium.Icon(color='green')))
+
+for lt, ln in zip(lat, lon):
+     fg.add_child(folium.Marker(location=[lt, ln], popup="Oh Hello!", icon=folium.Icon(color='green')))
+
 
 map.add_child(fg)
 
