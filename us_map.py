fdb108f3e8f87d28ba9e32d276e10e95936f4e62
Daniel Lessenden <dless27@gmail.com>
2021-01-02 14:05:23 -0600

    Add a child/marker

--- us_map.py
+++ us_map.py
@@ -1,4 +1,10 @@
 import folium
 
 map = folium.Map(location=[38, -99], zoom_start=6, tiles="Stamen Terrain")
+
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[38, -99], popup="Oh Hello!", icon=folium.Icon(color='green')))
+
+map.add_child(fg)
+
 map.save("us_map.html")
