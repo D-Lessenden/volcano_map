322b2a7104b9567c59ae336dc998b233721d4f88
Daniel Lessenden <dless27@gmail.com>
2021-01-02 14:47:00 -0600

    Add color coding for elevation

--- us_map.py
+++ us_map.py
@@ -4,18 +4,24 @@ import pandas
 data = pandas.read_csv("volcano.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+def elev_color(elev):
+    if elev < 2000:
+        return 'green'
+    elif elev > 3000:
+        return 'red'
+    else:
+        return 'orange'
 
 
 map = folium.Map(location=[38, -99], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-# for coords in [[38, -99], [37, -97]]:
-#     fg.add_child(folium.Marker(location=coords, popup="Oh Hello!", icon=folium.Icon(color='green')))
-
-for lt, ln in zip(lat, lon):
-     fg.add_child(folium.Marker(location=[lt, ln], popup="Oh Hello!", icon=folium.Icon(color='green')))
-
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+'m', icon=folium.Icon(color=elev_color(el))))
+   
 
 map.add_child(fg)
 
