ad1d56de66db62e60e2ecaa1f0dcdb78e23bfec3
Daniel Lessenden <dless27@gmail.com>
2021-01-02 14:17:23 -0600

    Add iterating loop

--- us_map.py
+++ us_map.py
@@ -3,7 +3,9 @@ import folium
 map = folium.Map(location=[38, -99], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[38, -99], popup="Oh Hello!", icon=folium.Icon(color='green')))
+
+for coords in [[38, -99], [37, -97]]:
+    fg.add_child(folium.Marker(location=coords, popup="Oh Hello!", icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
