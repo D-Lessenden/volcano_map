8cfe3e8dfed2e9abf84318cb9ba5062b4c0a1baa
Daniel Lessenden <dless27@gmail.com>
2021-01-02 15:23:47 -0600

    Add layer control for population and volcanoes

--- us_map.py
+++ us_map.py
@@ -17,15 +17,20 @@ def elev_color(elev):
 
 map = folium.Map(location=[38, -99], zoom_start=6, tiles="Stamen Terrain")
 
-fg = folium.FeatureGroup(name="My Map")
+fg_vol = folium.FeatureGroup(name="Volcanoes")
+fg_pop = folium.FeatureGroup(name="Population")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el)+'m', fill_color=elev_color(el), color = 'grey', fill_opacity=0.7))
+    fg_vol.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el)+'m', fill_color=elev_color(el), color = 'grey', fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+fg_pop.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
 style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 20000000 
 else 'orange' if 20000000 <= x['properties']['POP2005'] < 100000000 else 'red'}))
 
-map.add_child(fg)
+
+map.add_child(fg_vol)
+map.add_child(fg_pop)
+
+map.add_child(folium.LayerControl())
 
 map.save("us_map.html")
