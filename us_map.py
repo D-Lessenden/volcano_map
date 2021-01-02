f9acc433b7a73c05deb6901f717ac9ca29d2c956
Daniel Lessenden <dless27@gmail.com>
2021-01-02 14:54:34 -0600

    Change marker type

--- us_map.py
+++ us_map.py
@@ -20,8 +20,9 @@ map = folium.Map(location=[38, -99], zoom_start=6, tiles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+'m', icon=folium.Icon(color=elev_color(el))))
-   
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el)+'m', fill_color=elev_color(el), color = 'grey', fill_opacity=0.7))
+    #    fg.add_child(folium.CircleMarker(location=[lt, ln], raduis = 6, popup=str(el)+'m', fill_color=elev_color(el), color = 'grey', fill_opacity=0.7))
+
 
 map.add_child(fg)
 
