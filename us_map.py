cadec9dc43698be2fdbf11da095d5ab5307a16da
Daniel Lessenden <dless27@gmail.com>
2021-01-02 14:00:06 -0600

    Update map

--- us_map.py
+++ us_map.py
@@ -1,4 +1,4 @@
 import folium
 
- map = folium.Map(location=[38, -99], zoom_start=6)
- map.save("us_map.html")
+map = folium.Map(location=[38, -99], zoom_start=6, tiles="Stamen Terrain")
+map.save("us_map.html")
